9f85f412887fa82ed63913ddee6bec46c537b568
Matt Pope <UnknownGuardian@gmail.com>
2019-10-18 13:15:46 -0600

    Fixed typo and memory leak

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ type DelayedCall = {
   tickToExecute: number;
   callback:Function;
 }
+const TAG = "Metronome";
 
 class Metronome {
   // don't do work when there is nothing to do
@@ -30,7 +31,7 @@ class Metronome {
       const call = this._callbacks[i];
       if(call.tickToExecute == this._currentTick){
         await call.callback();
-        this._callbacks.slice(i, 1);
+        this._callbacks.splice(i, 1);
         i--;
       }
     }
@@ -41,6 +42,7 @@ class Metronome {
   // halt until resolved
   private async sleep() {
     if(this._callbacks.length == 0) {
+      console.log(TAG, "SLEEPING");
       await new Promise((resolve) => {
         this._sleepResolve = resolve;
       });
@@ -49,12 +51,14 @@ class Metronome {
 
   private async awake() {
     if(this._sleepResolve) {
+      console.log(TAG, "WAKING");
       this._sleepResolve();
       this._sleepResolve = null;
     }
   }
 
   setTimeout(callback:Function, ticks:number) {
+    console.log(TAG, "ADD delayed function in", ticks);
     this._callbacks.push({
       callback,
       tickToExecute: this._currentTick + ticks
